382bd3cbec4ee72140fb9783bc141a64fe5d6164
Kaushal Kumar Munda <kaushalkumarmunda@gmail.com>
2024-07-01 04:10:15 +0530

    first commit

--- signup page/signup.js
+++ signup page/signup.js
@@ -10,3 +10,31 @@ function togglePassword() {
     document.getElementById("toggleBtn").textContent = "Show";
   }
 }
+
+//Signin form input field label animation
+document.addEventListener("DOMContentLoaded", function () {
+  const inputFields = document.querySelectorAll(".formInput");
+
+  // Function to check input field state
+  function checkInputState(inputField) {
+    const inputContainer = inputField.parentElement;
+    if (inputField.value !== "") {
+      inputContainer.classList.add("active");
+    } else {
+      inputContainer.classList.remove("active");
+    }
+  }
+
+  // Event listeners for focus and blur
+  inputFields.forEach(function (inputField) {
+    inputField.addEventListener("focus", function () {
+      inputField.parentElement.classList.add("active");
+    });
+
+    inputField.addEventListener("blur", function () {
+      checkInputState(inputField);
+    });
+
+    checkInputState(inputField);
+  });
+});
